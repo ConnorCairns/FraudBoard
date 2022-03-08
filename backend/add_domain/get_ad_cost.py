4a3422011a820ceaad95d379943e2daa34bb6330
ConnorCairns <connor.f.cairns@gmail.com>
2022-03-08 14:02:45 +0000

    ad cost

--- backend/add_domain/get_ad_cost.py
+++ backend/add_domain/get_ad_cost.py
@@ -6,6 +6,14 @@ API_KEY = os.environ.get("SEOREVIEWTOOLS_API_KEY")
 WEBSITE_TRAFFIC_URL = lambda url: f"https://api.seoreviewtools.com/website-traffic/?url={url}&key={API_KEY}"
 WEBSITE_TRAFFIC_ORGANIC_PAID_URL = lambda url: f"https://api.seoreviewtools.com/website-traffic-organic-paid/?url={url}&location=United States&hl=English&key={API_KEY}"
 
+#From https://www.statista.com/statistics/242549/digital-ad-market-share-of-major-ad-selling-companies-in-the-us-by-revenue/
+#Google
+GOOGLE_PERCENTAGE = 28.9
+
+#Facebook
+FACEBOOK_PERCENTAGE = 24.9
+
+
 def handler(url):
     res = requests.get(WEBSITE_TRAFFIC_ORGANIC_PAID_URL(url))
 
