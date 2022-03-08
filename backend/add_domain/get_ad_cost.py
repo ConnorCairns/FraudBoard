19e56f5c15eb7965dfe462a8a2d164a5e04548e4
ConnorCairns <connor.f.cairns@gmail.com>
2022-03-08 14:23:51 +0000

    estimated with fb for now

--- backend/add_domain/get_ad_cost.py
+++ backend/add_domain/get_ad_cost.py
@@ -3,23 +3,33 @@ import os
 
 API_KEY = os.environ.get("SEOREVIEWTOOLS_API_KEY")
 
-WEBSITE_TRAFFIC_URL = lambda url: f"https://api.seoreviewtools.com/website-traffic/?url={url}&key={API_KEY}"
-WEBSITE_TRAFFIC_ORGANIC_PAID_URL = lambda url: f"https://api.seoreviewtools.com/website-traffic-organic-paid/?url={url}&location=United States&hl=English&key={API_KEY}"
 
-#From https://www.statista.com/statistics/242549/digital-ad-market-share-of-major-ad-selling-companies-in-the-us-by-revenue/
-#Google
-GOOGLE_PERCENTAGE = 28.9
+def WEBSITE_TRAFFIC_URL(
+    url): return f"https://api.seoreviewtools.com/website-traffic/?url={url}&key={API_KEY}"
 
-#Facebook
-FACEBOOK_PERCENTAGE = 24.9
+
+def WEBSITE_TRAFFIC_ORGANIC_PAID_URL(
+    url): return f"https://api.seoreviewtools.com/website-traffic-organic-paid/?url={url}&location=United States&hl=English&key={API_KEY}"
+
+
+FACEBOOK_AVG_CPC = 1.72
 
 
 def handler(url):
-    res = requests.get(WEBSITE_TRAFFIC_ORGANIC_PAID_URL(url))
+    estimated_cost = 0
 
-    print(res.json())
+    try:
+        res = requests.get(WEBSITE_TRAFFIC_ORGANIC_PAID_URL(url))
 
-    return 0
+        paid_traffic = res.json()['data']['data'][0]['paid']
+        estimated_cost = paid_traffic * FACEBOOK_AVG_CPC
+
+    except Error as e:
+        print(e)
+        pass
+
+    return estimated_cost
+
 
 if __name__ == '__main__':
-    handler("jungleboypacks.com")
+    print(handler("vland-official.com"))
