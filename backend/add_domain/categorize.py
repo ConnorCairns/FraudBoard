e97d0b0d10057c55d11f002caaf0c3154190e5bd
ConnorCairns <connor.f.cairns@gmail.com>
2022-03-15 11:18:19 +0000

    tfidf

--- backend/add_domain/categorize.py
+++ backend/add_domain/categorize.py
@@ -2,37 +2,30 @@ import boto3
 import tempfile
 import scrape_text
 from joblib import load
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 BUCKET = "fraud-dashboard-bucket"
 MODEL = "brc-clustering.joblib"
+TFIDF = "tfidf.joblib"
 
-
-def get_classifier():
+def get_file_from_s3(obj):
     s3 = boto3.client('s3')
 
     with tempfile.TemporaryFile() as data:
-        s3.download_fileobj(BUCKET, MODEL, data)
+        s3.download_fileobj(BUCKET, obj, data)
         data.seek(0)
         return load(data)
-
 
 def identity(token):
     return token
 
-
 def handler(tokens):
-    classifier = get_classifier()
+    classifier = get_file_from_s3(MODEL)
+    tfidf = get_file_from_s3(TFIDF)
 
-    tfidf = TfidfVectorizer(max_df=0.8, min_df=1, stop_words='english', use_idf=True,
-                            tokenizer=identity, preprocessor=identity, token_pattern=None)
+    pred = classifier.predict(tfidf.fit_transform([tokens]))
 
-    pred = classifier.predict(tfidf.fit_transform(tokens))
-
-    print(pred)
-
-    return 0
+    return pred
 
 
 if __name__ == '__main__':
-    handler(scrape_text.handler("https://vland-official.com"))
+    print(handler(scrape_text.handler("https://vland-official.com")))
